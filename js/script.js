7b735e2ebccf7b8c518e91773b0882bea309c674
Eugene Martynenko <feliz22@ukr.net>
2025-05-27 16:32:44 +0300

    Feat: realize js (user interaction)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 'use strict';
 
+function userInterection () {
+    let userName = prompt('Enter your name');
+    userName = userName.trim();
+    const userChoice = confirm('Show Greeting?');
+    if (userChoice && userName) {
+        alert(`Hello, ${userName}! How are you?`);
+    } else if (userChoice) {
+        alert('You did not enter a name.');
+    }
+}
 
+userInterection ();
