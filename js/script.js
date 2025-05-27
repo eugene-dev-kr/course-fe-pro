205dc42563c7b894ea99b8774004206e91fb80f6
Eugene Martynenko <feliz22@ukr.net>
2025-05-27 15:58:07 +0300

    Feat: realize js (greeting output)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+
+function helloUser () {
+    let userName = prompt('What is your name?');
+    userName = userName.trim();
+    if(userName) {
+        alert(`Hello, ${userName}! How are you?`);
+    }else {
+        alert('You did not enter a name!');
+    }
+}
+
+helloUser();
