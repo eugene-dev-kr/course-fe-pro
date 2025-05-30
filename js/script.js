9c3a2278d6b5fbcc7cad5ecce8d2e7c1a0e1d987
Eugene Martynenko <feliz22@ukr.net>
2025-05-30 15:14:57 +0300

    Feat: Realize js (Mini-user registration in the browser)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+function userRegistration() {
+    const userName = String(prompt(`Введіть своє ім'я:`)).trim();
+    const userAge = Number(prompt(`Скільки вам років?`));
+    const userId = 4759384574302583457028345348905n;
+    let userBonusCode = null;
+    const userSecondName = undefined;
+    if (userAge <= 0 || Number.isNaN(userAge)) {
+        alert('Ваш вік некоректний!');
+        return;
+    }
+    let userParticipation = confirm('Ви хочете прийняти участь в акції?');
+    console.log(`Ім'я: ${userName}, тип: ${typeof userName}`);
+    console.log(`Вік: ${userAge}, тип: ${typeof userAge}`);
+    console.log(`Участь: ${userParticipation}, тип: ${typeof userParticipation}`);
+    console.log(`ID: ${userId}, тип: ${typeof userId}`);
+    console.log(`Бонуси: ${userBonusCode}, тип: ${typeof userBonusCode}`);
+    console.log(`Резервне ім'я: ${userSecondName}, тип: ${typeof userSecondName}`);
+    return alert(`Привіт, ${userName ? userName : userSecondName}!\nТвій вік: ${userAge}\nТвій статус: ${userParticipation ? 'участь підтверджено' : 'відмовився'}`);
+}
+
+userRegistration();
