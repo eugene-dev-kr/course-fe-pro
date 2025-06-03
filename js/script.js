a917a112e5ab1e4bbd081080425baf954a0cddab
Eugene Martynenko <feliz22@ukr.net>
2025-06-03 14:20:48 +0300

    Feat: Realize js (user personal card + friends list)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const user = {
+    name: 'Eugene',
+    age: 31,
+    isOnline: true,
+    friends: ['Vlad', 'Dima', 'Leon'],
+    settings: {
+        theme: 'light',
+        notifications: true
+    }
+}
+
+const firstFriendName = user.friends[0];
+const notificationsEnabled = user.settings.notifications;
+
+console.log(`Ім'я користувача: ${user.name}\nКількість друзів: ${user.friends.length}\nІм'я першого друга: ${firstFriendName}\nТема: ${user.settings.theme}\nСповіщення: ${notificationsEnabled ? 'Увімкнені' : 'Вимкнені'}`);
