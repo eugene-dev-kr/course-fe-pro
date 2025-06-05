c01b59d9e4ac6d58fb2f629f43699ca129aa0361
Eugene Martynenko <feliz22@ukr.net>
2025-06-05 15:47:45 +0300

    Feat: Realize js script (Convert hours to seconds)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+function hoursToSec() {
+    // Get string type input value
+    const inputHours = prompt('Enter the number of hours:');
+
+    // if input value empty or return null (cancel button) - exit
+    if (inputHours === null || inputHours.trim() === "") {
+        alert('You do not enter anything');
+        return;
+    }
+
+    // if input value not empty, convert to number types
+    const hours = Number(inputHours);
+
+    // if hours not 0 or < 0 and hours not NaN, convert to seconds
+    if (hours > 0 && !Number.isNaN(hours)) {
+        const seconds = hours * 3600;
+        alert(`${hours} hour's = ${seconds} seconds`);
+    } else alert('You did not enter a number', );
+
+}
+
+hoursToSec();
