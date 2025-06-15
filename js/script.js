6e9e57fe475187a5685b1bb64d355b7a3870ab6e
Eugene Martynenko <feliz22@ukr.net>
2025-06-15 22:02:10 +0300

    Feat: Realize js script (generation of a random key from a string characters).

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const generateKey = (lengthKey, chars) => {
+    let result = '';
+    for (let i = 1; i <= lengthKey; i++) {
+        let oneRandomNum = Math.floor(Math.random() * chars.length);
+        result += chars.charAt(oneRandomNum);
+    }
+    return result;
+}
+
+const key = generateKey(8,characters);
+
+console.log(key);
