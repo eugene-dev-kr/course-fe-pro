44c34c6c363a42ed4865774b62ecd68e205f86e4
Eugene Martynenko <feliz22@ukr.net>
2025-06-18 00:56:07 +0300

    Feat: Implemented deletion of an array element by a given number (js script)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 'use strict';
 
+const arrInput = [1,2,3,4,5,6,7];
 
+
+const removeElement = (arr, delNum) => {
+
+    if(delNum > arr.length) {
+        return `Такого елементу не існує! Довжина массиву: ${arr.length}.`;
+    }else {
+        for (let i = 0; i < arr.length; i++) {
+            if(arr[i] === delNum) {
+                arrInput.splice(i, 1);
+            }
+        }
+    }
+    return arrInput;
+}
+
+console.log(removeElement(arrInput,5));
+console.log(removeElement(arrInput,10));
