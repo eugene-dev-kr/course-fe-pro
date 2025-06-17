551fbee80465a8687d13b0e7c5fd74d64756c2fb
Eugene Martynenko <feliz22@ukr.net>
2025-06-18 00:32:19 +0300

    Feat: Implemented is check positive number function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const arrayEx1 = [1, 2, 3, -1, -2, -3];
+const arrayEx2 = [-1, -2, -3];
+const arrayEx3 = [];
+
+const isPositiveNumber = (arr) => {
+    const arrResult = [];
+
+    if(arr.length === 0) return 'Ви передали пустий масив!'
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] >= 0) arrResult.push(arr[i]);
+    }
+
+    return arrResult.length > 0 ? arrResult : null;
+}
+
+
+console.log(isPositiveNumber(arrayEx1)); // [1, 2, 3]
+console.log(isPositiveNumber(arrayEx2)); // null
+console.log(isPositiveNumber(arrayEx3)); // Ви передали пустий масив!
