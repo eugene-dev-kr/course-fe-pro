46cc00fd0f64502d32f5369ed971d64c13d78934
Eugene Martynenko <feliz22@ukr.net>
2025-06-19 12:47:56 +0300

    Feat: A function has been implemented that calculates the arithmetic mean of only the numeric elements of a given array

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+// 1
+const arrInput = ['Apple', true, NaN, 0, 1, 4, 6, 3, 'Melon'];
+
+const arithmeticMean = (arr) => {
+    let resultNums = 0;
+    let resultNumsCount = 0;
+    for (const item of arr) {
+        if (typeof item === 'number' && !Number.isNaN(item)) {
+            resultNums += item;
+            resultNumsCount++;
+        }
+    }
+    return resultNumsCount > 0 ? resultNums / resultNumsCount : null;
+}
+
+console.log(arithmeticMean(arrInput))
+
+
+
