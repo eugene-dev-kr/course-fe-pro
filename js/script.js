38d66e2519bd69d642e9e4e3282df68696d8c3af
Eugene Martynenko <feliz22@ukr.net>
2025-06-22 14:08:40 +0300

    Feat: Implemented a function to remove the first element from an array

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+// 1. function arrShift() > method shift()
+const arrInput = [1, 2, 3];
+
+const arrShift = (arr) => {
+    const delElement = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        arr[i - 1] = arr[i];
+    }
+    arr.length -= 1;
+    return delElement;
+}
+
+const deletedElement = arrShift(arrInput);
+
+console.log(deletedElement);
+console.log(arrInput);
+
