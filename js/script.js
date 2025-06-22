824fe37635c9da1e8e723879331e5fa3e1917ad3
Eugene Martynenko <feliz22@ukr.net>
2025-06-22 18:34:22 +0300

    Feat: (lastIndexOf) Implemented a function to search for an element in an array from the end and return its first index

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
 'use strict';
 
 // 1 function funcIndexOf > method indexOf
-const arrInput = ['Phone', 'Email', 'Password', 'Id', 'Name'];
+// const arrInput = ['Phone', 'Email', 'Password', 'Id', 'Name'];
+//
+// const funcIndexOf = (arrInput, el, startIndex) => {
+//
+//     if(startIndex === undefined) startIndex = 0;
+//     if(startIndex >= arrInput.length) return -1;
+//     if(startIndex < 0) {
+//         startIndex = arrInput.length + startIndex;
+//         if(startIndex < 0) return -1;
+//     }
+//
+//     for(let i = startIndex; i < arrInput.length; i++) {
+//         if(el === arrInput[i]) return i;
+//     }
+//     return -1;
+// }
+//
+// console.log(funcIndexOf(arrInput, 'Email'));
+// console.log(funcIndexOf(arrInput, 'Email',1));
+// console.log(funcIndexOf(arrInput, 'Email',2));
+// console.log(funcIndexOf(arrInput, 'Email',-3));
+// console.log(funcIndexOf(arrInput, 'Email',7));
 
-const funcIndexOf = (arrInput, el, startIndex) => {
 
-    if(startIndex === undefined) startIndex = 0;
-    if(startIndex >= arrInput.length) return -1;
-    if(startIndex < 0) {
-        startIndex = arrInput.length + startIndex;
-        if(startIndex < 0) return -1;
-    }
-
-    for(let i = startIndex; i < arrInput.length; i++) {
-        if(el === arrInput[i]) return i;
-    }
-    return -1;
-}
-
-console.log(funcIndexOf(arrInput, 'Email'));
-console.log(funcIndexOf(arrInput, 'Email',1));
-console.log(funcIndexOf(arrInput, 'Email',2));
-console.log(funcIndexOf(arrInput, 'Email',-3));
-console.log(funcIndexOf(arrInput, 'Email',7));
+// 2 function funcLastIndexOf > method lastIndexOf
+// const arrInput = ['dog', 'cat', 'horse', 'dog', 'elephant'];
+//
+// const funcLastIndexOf = (arrInput, el, startIndex) => {
+//     if(startIndex === undefined ) startIndex = arrInput.length - 1;
+//     if(startIndex >= arrInput.length) startIndex = arrInput.length - 1;
+//     if(startIndex < 0) {
+//         startIndex = arrInput.length + startIndex;
+//         if (startIndex < 0) return -1;
+//     }
+//     for(let i = startIndex; i >= 0; i--) {
+//         if(el === arrInput[i]) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+//
+// console.log(funcLastIndexOf(arrInput, 'dog'));
+// console.log(funcLastIndexOf(arrInput, 'dog', 2));
+// console.log(funcLastIndexOf(arrInput, 'dog', 6));
+// console.log(funcLastIndexOf(arrInput, 'dog', -15));
