fba0219e1a01f1c147426313b977495be65042fa
Eugene Martynenko <feliz22@ukr.net>
2025-06-22 15:47:40 +0300

    Feat: Implemented the function of reversing array elements.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
 // 1. function arrShift() > method shift()
-const arrInput = [1, 2, 3];
+console.log(`---------------function arrShift() > method shift()----------------`);
+
+const arrInput1 = [1, 2, 3];
+console.log('Input array:', arrInput1);
 
 const arrShift = (arr) => {
     const delElement = arr[0];
@@ -12,8 +15,30 @@ const arrShift = (arr) => {
     return delElement;
 }
 
-const deletedElement = arrShift(arrInput);
+const deletedElement = arrShift(arrInput1);
 
-console.log(deletedElement);
-console.log(arrInput);
+console.log('Deleted element:', deletedElement);
+console.log('Changed array:', arrInput1);
+console.log('\n');
 
+// 2. function arrReverse() > method reverse()
+console.log(`-------------function arrReverse() > method reverse()--------------`);
+const arrInput2 = ['Banana', 'Apple', 'Melon', 'Pineapple', 'Strawberry','Tomato', 'Cucumber'];
+console.log('Input array:', arrInput2);
+
+const arrReverse = (arr) => {
+
+    for(let i = 0; i < arr.length / 2; i++) {
+
+        let tmpLeftEl = arr[i];
+        let tmpRightEl = arr.length - 1 - i;
+        arr[i] = arr[tmpRightEl];
+        arr[tmpRightEl] = tmpLeftEl;
+
+    }
+    return arr;
+}
+
+arrReverse(arrInput2);
+
+console.log('Output array:', arrInput2);
