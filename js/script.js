55fc4bc7b930fc6d1fd0753006f873ecd1ed6a2c
Eugene Martynenko <feliz22@ukr.net>
2025-06-23 15:02:32 +0300

    Feat: (find) Implemented a function to search for an element based on a specific condition

--- js/script.js
+++ js/script.js
@@ -47,3 +47,19 @@
 // console.log(funcLastIndexOf(arrInput, 'dog', 2));
 // console.log(funcLastIndexOf(arrInput, 'dog', 6));
 // console.log(funcLastIndexOf(arrInput, 'dog', -15));
+
+// 3 function funcLastIndexOf > method lastIndexOf
+// const animals = ['dog', 'cat', 'horse', 'elephant'];
+//
+// const funcFind = (arr, callBack) => {
+//     for(let i = 0; i < arr.length; i++) {
+//         if(callBack(arr[i], i, arr)) return arr[i];
+//     }
+//     return undefined;
+// }
+//
+// const resultFind1 = funcFind(animals, (item) => item === "dog");
+// const resultFind2 = funcFind(animals, (item, index) => index === 2);
+// const resultFind3 = funcFind(animals, (item) => item === "cow");
+//
+// console.log(`${resultFind1}\n${resultFind2}\n${resultFind3}\n`);
