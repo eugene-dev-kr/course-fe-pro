7149af360ef2f8285e6f6c9190bdb8f9bcc402d7
Eugene Martynenko <feliz22@ukr.net>
2025-07-01 18:05:58 +0300

    Feat: Implemented function to find target key in object

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 'use strict';
 
+const data = {
+    id: 1,
+    name: "root",
+    meta: {
+        id: 2,
+        parent: {
+            id: 3,
+            name: "leaf",
+        },
+    },
+    array: [
+        { id: 4 },
+        { name: "node", children: [{ id: 5 }] },
+    ],
+};
+
+
+const findValuesByKey = (obj, key) => {
+    let resultArr = [];
+
+    for(let keyInner in obj) {
+        if( keyInner === key) resultArr.push(obj[keyInner]);
+        if(typeof obj[keyInner] === 'object') {
+            resultArr = resultArr.concat(findValuesByKey(obj[keyInner], key));
+        }
+    }
+    return resultArr;
+}
+
+console.log(findValuesByKey(data, 'id'));
