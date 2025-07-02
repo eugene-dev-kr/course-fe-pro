15e4d3260159e68e6d17461d41c855fb5c2c8cf0
Eugene Martynenko <feliz22@ukr.net>
2025-07-02 21:52:55 +0300

    Feat: Implemented function apply.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+//apply
+const obj = {
+    name: 'Eugene'
+}
+
+const tmpFunc = function(...args) {
+    console.log(this.name, ...args);
+}
+
+const apply = function (tmpFunc, context, args) {
+    context.tmp = tmpFunc;
+    const result = context.tmp(...args);
+    delete context.tmp;
+    return result;
+}
+
+apply(tmpFunc, obj, [1,2]);
+
