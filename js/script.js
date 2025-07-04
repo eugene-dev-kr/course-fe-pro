58fcdd6971b5e4957e32b8cf298e16e52870ead8
Eugene Martynenko <feliz22@ukr.net>
2025-07-04 18:17:42 +0300

    Feat: Implemented filter for select

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const select = document.getElementById('filter');
+const cards = document.querySelectorAll('.card');
+
+select.addEventListener('change', (event) => {
+    const selectedValue = event.target.value;
+
+    cards.forEach(card => {
+        const category = card.dataset.category;
+
+        if (selectedValue === 'all' || category === selectedValue) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
