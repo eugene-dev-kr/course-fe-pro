9e754b9c1b890178a01dec3825fe4ab01e55e39c
Eugene Martynenko <feliz22@ukr.net>
2025-07-04 15:24:56 +0300

    Feat: Implemented function bind.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const obj = {
     name: 'Eugene'
 }
 
-const tmpFunc = function(...args) {
+const tmpFunc1 = function(...args) {
     console.log(this.name, ...args);
 }
 
@@ -16,5 +16,26 @@ const apply = function (tmpFunc, context, args) {
     return result;
 }
 
-apply(tmpFunc, obj, [1,2]);
+apply(tmpFunc1, obj, [1,2]);
+
+// //bind
+const obj2 = {
+    name: 'Eugene'
+}
+
+const tmpFunc2 = function(...args) {
+    return this.name + ' ' + args;
+}
+
+const bind = function(tmpFunc, context, ...bindArgs) {
+    return function(...callArgs) {
+        context.tmp = tmpFunc;
+        const result = context.tmp(...bindArgs, ...callArgs);
+        delete context.tmp;
+        return result;
+    }
+};
+
+const binded = bind(tmpFunc2, obj2, [1,2])
+console.log(binded([5, 6]));
 
