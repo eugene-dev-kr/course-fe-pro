43e250a47efbb6f190c4abe8d041133a8ef84ed7
Eugene Martynenko <feliz22@ukr.net>
2025-07-25 17:51:49 +0300

    Feat: Implemented function constructor

--- js/Student.js
+++ js/Student.js
@@ -1,5 +1,81 @@
 'use strict';
 
+function Student(firstName, lastName, birthYear, lessonsCount = 25) {
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.lessonsCount = lessonsCount;
+    this.marks = new Array(lessonsCount); //оцінки
+    this.attendances = new Array(lessonsCount); // присутність (true/false)
+    this.currentLesson = 1;
+}
+
+Student.prototype.getAge = function() {
+    return new Date().getFullYear() - this.birthYear;
+}
+
+Student.prototype.maxMark = 100;
+Student.prototype.minMark = 0;
+
+Student.prototype._getCurrentLessonIndex = function() { // отримуємо індекс для роботи з масивом
+    return this.currentLesson - 1;
+}
+
+Student.prototype._setAttendance = function(isPresent) {
+    if(typeof isPresent !== 'boolean') throw new Error('isPresent should be a boolean');
+    if(this.currentLesson > this.lessonsCount) {
+        throw new Error(`lessons count must be less than or equal ${this.lessonsCount}`);
+    }
+    this.attendances[this._getCurrentLessonIndex()] = isPresent;
+    this.currentLesson += 1;
+}
+
+Student.prototype._calcAvgMark = function() {
+    const marksData = this.marks.reduce((acc, mark) => {
+        acc.sum += mark;
+        acc.count += 1;
+        return acc;
+    }, {sum: 0, count: 0});
+
+    return Math.floor(marksData.sum / marksData.count);
+}
+
+Student.prototype._calcAvgAttd = function() {
+    const {visited, count} = this.attendances.reduce((acc, attd) => {
+        if(attd) acc.visited += 1;
+        if(typeof attd === 'boolean') acc.count += 1;
+
+        return acc;
+    }, {visited: 0, count: 0});
+
+    return Number((visited / count).toFixed(2));
+}
+
+Student.prototype.present = function() {
+    this._setAttendance(true);
+}
+
+Student.prototype.absent = function() {
+    this._setAttendance(false);
+}
+
+Student.prototype.setMark = function(mark) {
+    if(typeof mark !== "number" || isNaN(mark) ) throw new Error('mark should be a number');
+    if(mark < this.minMark || mark > this.maxMark) throw new Error('mark must be less than 100 and more than 0');
+    const currentLessonIndex = this._getCurrentLessonIndex() - 1;
+    if(this.attendances[currentLessonIndex]) {
+        this.marks[currentLessonIndex] = mark;
+    } else {
+        console.warn('You cannot set mark for this lesson');
+    }
+}
+Student.prototype.summary = function () {
+    return {
+        avgMark: this._calcAvgMark(),
+        avgAttd: this._calcAvgAttd(),
+    }
+}
 
 
 export default Student;
--- js/app.js
+++ js/app.js
@@ -2,3 +2,37 @@
 
 import Student from './Student.js';
 
+const student1 = new Student('Eugene', 'Martynenko', 1993);
+const student2 = new Student('Vlad', 'Ivanenkо', 1995);
+
+student1.present(); // заняття 1 — присутній
+student1.present(); // заняття 2 — присутній
+student1.present();  // заняття 3 — присутній
+
+// Пробуємо поставити оцінки
+student1.setMark(90); // заняття 1 — можна, бо був присутній
+student1.setMark(95); // заняття 2 — можна, бо був присутній
+student1.setMark(85); // заняття 3 — не можна, бо був відсутній
+
+
+student2.present();  // заняття 1 — присутній
+student2.absent();   // заняття 2 — відсутній
+student2.present();  // заняття 3 — присутній
+
+student2.setMark(100); // заняття 1 — можна
+student2.setMark(50);  // заняття 2 — не можна
+student2.setMark(80);  // заняття 3 — можна
+
+console.log('Student 1 summary:', student1.summary());
+// { avgMark: 92, avgAttd: 0.67 }
+
+console.log('Student 2 summary:', student2.summary());
+// { avgMark: 90, avgAttd: 0.67 }
+
+console.log('Student 1 age:', student1.getAge()); // Наприклад: 2025 - 2000 = 25
+
+console.log(student1.marks);
+// [90, 95, empty × 23]
+
+console.log(student1.attendances);
+// [true, true, false, empty × 22]
