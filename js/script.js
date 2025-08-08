89563e460550c7db6f7f29e4564c6a12126bbddd
Eugene Martynenko <feliz22@ukr.net>
2025-08-08 19:18:26 +0300

    Feat: Implemented Class for working with transition history.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 'use strict';
 
+class HistoryTracker {
+    constructor() {
+        this.historyDataUrl = [];
+
+        window.addEventListener('popstate', (event) => {
+            console.log(event.state);
+        })
+    }
+
+    push(url) {
+        window.history.pushState(
+            {url: url},
+            '',
+            url
+        )
+        this.historyDataUrl.push(window.location.href);
+    }
+
+    back() {
+        window.history.back();
+    }
+
+    getDataHistoryUrl() {
+        return this.historyDataUrl;
+    }
+}
+
+const track = new HistoryTracker();
+
+track.push('/catalog');
+track.push('/about');
+track.push('/contact');
+
+track.back();
+
+console.log(track.getDataHistoryUrl());
