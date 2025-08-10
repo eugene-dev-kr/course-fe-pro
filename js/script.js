9d666334abec8b4f01930a2e07c94071c9688fee
Eugene Martynenko <feliz22@ukr.net>
2025-08-10 22:20:48 +0300

    Feat: Implemented Class Navigation for working with transition history

--- js/script.js
+++ js/script.js
@@ -1,2 +1,51 @@
 'use strict';
 
+const navItemsCollection = document.querySelectorAll('.nav-item');
+
+class Navigation {
+
+    constructor(items) {
+
+        this.navItems = items;
+        this.#updateNavActiveItem();
+
+        this.navItems.forEach((item) => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault();
+                const attrItemHref = item.getAttribute('href');
+                window.history.pushState({}, '', attrItemHref)
+                this.#updateNavActiveItem();
+
+            })
+        })
+        window.addEventListener('popstate', (event) => {
+            this.#updateNavActiveItem();
+        })
+    }
+
+    #updateNavActiveItem() {
+
+        const currentPath = window.location.pathname;
+
+        this.#removeActiveClassNavItem();
+
+        this.navItems.forEach((item) => {
+            const itemHref = item.getAttribute('href');
+
+            const normalizedPath = currentPath === '/' ? '/home' : currentPath;
+
+            if (itemHref === normalizedPath) {
+                item.classList.add('active');
+            }
+        })
+    }
+
+    #removeActiveClassNavItem() {
+        for (const item of this.navItems) {
+            item.classList.remove('active');
+        }
+    }
+}
+
+const nav = new Navigation(navItemsCollection);
+
